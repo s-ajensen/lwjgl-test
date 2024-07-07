31d8609ce96c7974fe53faa46b86c58ce0e846bd
Alex Jensen <alex@endure.io>
2024-07-06 23:08:19 -0400

    working clojure triangle

--- src/clj/lwjgl_test/main.clj
+++ src/clj/lwjgl_test/main.clj
@@ -1,4 +1,4 @@
-(ns lwjgl-test.repl
+(ns lwjgl-test.main
   (:import [org.lwjgl.glfw Callbacks GLFW GLFWErrorCallback GLFWVidMode]
            [org.lwjgl.opengl GL GL11]
            [org.lwjgl.system MemoryStack MemoryUtil]))
@@ -30,9 +30,19 @@
 
 (defn loop-fn [window-atom]
   (GL/createCapabilities)
+
   (GL11/glClearColor 0.0 0.0 0.0 0.0)
+
   (while (not (GLFW/glfwWindowShouldClose @window-atom))
     (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+
+    (GL11/glColor3f 1.0 1.0 1.0)
+    (GL11/glBegin GL11/GL_TRIANGLES)
+    (GL11/glVertex2f -0.5 -0.5)
+    (GL11/glVertex2f 0.5 -0.5)
+    (GL11/glVertex2f 0.0 0.5)
+    (GL11/glEnd)
+
     (GLFW/glfwSwapBuffers @window-atom)
     (GLFW/glfwPollEvents)))
 
